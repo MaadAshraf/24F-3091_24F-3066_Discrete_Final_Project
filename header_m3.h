e67b0abc50332619c87cfe175bacce0dd3bd4c3a
Ali Hassaan <hassaan.alich888@gmail.com>
2025-11-25 22:53:13 +0500

    Update header_m3.h

--- header_m3.h
+++ header_m3.h
@@ -1,62 +1,24 @@
-//#ifndef header_m3_h
-//#define header_m3_h
-//
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//void dfsCheck(int x, string need[]);
-//void checkprereq(int x);
-//
-//#include "m3.cpp" 
-//
-//int main()
-//{
-//    cout << "Enter number of courses : ";
-//    cin >> n;
-//
-//    cin.ignore();
-//
-//    for (int i = 0; i < n; i++)
-//    {
-//        cout << "Enter course " << i + 1 << " : ";
-//        getline(cin, courses[i]);
-//    }
-//
-//    cout << "Enter prerequisite matrix (0 or 1):" << endl;
-//
-//    for (int i = 0; i < n; i++)
-//    {
-//        for (int j = 0; j < n; j++)
-//        {
-//            cin >> graphC[i][j];
-//        }
-//    }
-//
-//    cout << "Enter completed status for each course (0/1):" << endl;
-//
-//    for (int i = 0; i < n; i++)
-//    {
-//        cin >> done[i];
-//    }
-//
-//    while (true)
-//    {
-//        int x;
-//        cout << "\nEnter course index to check (-1 to exit): ";
-//        cin >> x;
-//
-//        if (x == -1)
-//        {
-//            break;
-//        }
-//
-//        if (x >= 0 && x < n)
-//        {
-//            checkPrereq(x);
-//        }
-//    }
-//
-//    return 0;
-//}
-//
-//#endif
+#ifndef header_m3_h
+#define header_m3_h
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class m3
+{
+public:
+    string courses[50];
+    bool graphC[50][50];
+    bool done[50];
+    bool used[50];
+    int n;
+
+    void setcourses(int num, string arr[]);
+    void setgraph(bool g[50][50]);
+    void setdone(bool d[50]);
+    void dfscheck(int x, string need[]);
+    void checkprereq(int x);
+};
+
+#endif
