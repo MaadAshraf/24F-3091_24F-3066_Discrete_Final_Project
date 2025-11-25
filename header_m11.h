83e7e34f4b03c529efc6a3ba7da9d09b462f472f
Ali Hassaan <hassaan.alich888@gmail.com>
2025-11-25 23:11:38 +0500

    Update header_m11.h

--- header_m11.h
+++ header_m11.h
@@ -1,17 +1,27 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//void coursepreqs();
-//void setsModule();
-//void consistencyChecker();
-//
-//void menu();
-//
-//#include"m11.cpp"
-//
-//int main()
-//{
-//    menu();
-//    return 0;
-//}
+#ifndef HEADER_M11_H
+#define HEADER_M11_H
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+#include "header_m1.h"
+#include "header_m3.h"
+#include "header_m5.h"
+#include "header_m7.h"
+#include "header_m9.h"
+
+class m11
+{
+public:
+    void menu();
+};
+
+int main()
+{
+    m11 obj;
+    obj.menu();
+    return 0;
+}
+
+#endif
