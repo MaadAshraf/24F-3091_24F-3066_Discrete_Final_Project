a9201cf918b019e5179fa7c193f990409c2e86a7
Ali Hassaan <hassaan.alich888@gmail.com>
2025-11-25 22:54:47 +0500

    Update header_m9.h

--- header_m9.h
+++ header_m9.h
@@ -1,26 +1,25 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//const int MAX = 50;
-//
-//string students[MAX];
-//string courses[MAX];
-//int nStudents;
-//int nCourses;
-//
-//int enroll[MAX][MAX];
-//int nEnroll[MAX];
-//
-//int graph[MAX][MAX];
-//
-//void checkConflicts();
-//void consistencyChecker();
-//
-//#include"m9.cpp"
-//
-//int main()
-//{
-//    consistencyChecker();
-//    return 0;
-//}
+#ifndef header_m9_h
+#define header_m9_h
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+const int MAX = 50;
+
+string students[MAX];
+string courses[MAX];
+int nStudents;
+int nCourses;
+int enroll[MAX][MAX];
+int nEnroll[MAX];
+int graph[MAX][MAX];
+
+class m9
+{
+public:
+    void checkconflicts(string students[], int nStudents, string courses[], int nCourses, int enroll[][MAX], int nEnroll[], int graph[][MAX]);
+    void consistencychecker();
+};
+
+#endif
